569d30bcae75f4445d7853c32cdc607a8261b082
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-10-02 15:23:26 +0200

    add counter

---
+++ web/counter/script.js
@@ -0,0 +1,11 @@
+let value = 0;
+
+function plus() {
+    value++;
+    document.getElementById("value").innerText = "value = " + value;
+}
+
+function moins() {
+    value--;
+    document.getElementById("value").innerText = "value = " + value;
+}
